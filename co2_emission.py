18f3ccad1a3aca02d5d7b3d2cc6c66a1ea2d1812
Seungeun <seungeun2003@gmail.com>
2026-07-26 21:51:46 +0900

    Impelement co2_emission.py

--- co2_emission.py
+++ co2_emission.py
@@ -5,53 +5,56 @@ st.title("🌱 탄소 절감 계산기")
 actions = {
     "electric": {
         "label": "⚡ 전기 절약",
+        "emission_factor": 414.9,
         "items": {
             "air": {
                 "label": "에어컨",
                 "unit": "시간",
-                "factor": 0.8
+                "factor": 1.3
             },
             "tv": {
                 "label": "TV",
                 "unit": "시간",
-                "factor": 0.1
+                "factor": 0.12
             },
             "light": {
                 "label": "조명",
                 "unit": "시간",
-                "factor": 0.01
+                "factor": 0.03
             },
-            "computer": {
-                "label": "컴퓨터",
+            "laptop": {
+                "label": "노트북",
                 "unit": "시간",
-                "factor": 0.02
+                "factor": 0.025
             }
         }
     },
 
     "trans": {
         "label": "🚗 운송",
+        "emission_factor": 1,
         "items": {
             "car": {
                 "label": "자동차(휘발유)",
-                "unit": "km",
-                "factor": 0.02
+                "unit": "L",
+                "factor": 19.731
             },
             "car_2": {
                 "label": "자동차(경유)",
-                "unit": "km",
-                "factor": 0.02
+                "unit": "L",
+                "factor": 20.090
             },
             "plane": {
                 "label": "비행기",
                 "unit": "시간",
-                "factor": 0.03
+                "factor": 900 * 285
             }
         }
     },
 
     "meal": {
         "label": "🥗 채식",
+        "emission_factor": 1,
         "items": {
             "beef": {
                 "label": "소고기",
@@ -78,6 +81,7 @@ actions = {
 
     "recycle": {
         "label": "♻️ 재활용",
+        "emission_factor": 1,
         "items": {
             "can": {
                 "label": "캔",
@@ -113,6 +117,7 @@ for category_key, category_info in actions.items():
                 selected_actions[item_key] = {
                     "value": value,
                     "factor": item_info["factor"],
+                    "emission_factor": category_info["emission_factor"],
                     "label": item_info["label"],
                     "unit": item_info["unit"]
                 }
@@ -121,6 +126,6 @@ if st.button("계산하기"):
     total = 0
 
     for action in selected_actions.values():
-        total += action["value"] * action["factor"]
+        total += action["value"] * action["factor"] * action["emission_factor"]
 
     st.success(f"총 탄소 절감량 : {total:.2f} kgCO₂")
