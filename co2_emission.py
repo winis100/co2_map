9922dfb7419301a395176b39df1d573f4d223faf
Seungeun <seungeun2003@gmail.com>
2026-07-28 20:40:28 +0900

    Implement co2_emission.py

--- co2_emission.py
+++ co2_emission.py
@@ -1,131 +1,200 @@
 import streamlit as st
 
-st.title("🌱 탄소 절감 계산기")
+st.title("🌱 탄소맵")
 
-actions = {
-    "electric": {
-        "label": "⚡ 전기 절약",
-        "emission_factor": 414.9,
-        "items": {
-            "air": {
-                "label": "에어컨",
-                "unit": "시간",
-                "factor": 1.3
-            },
-            "tv": {
-                "label": "TV",
-                "unit": "시간",
-                "factor": 0.12
-            },
-            "light": {
-                "label": "조명",
-                "unit": "시간",
-                "factor": 0.03
-            },
-            "laptop": {
-                "label": "노트북",
-                "unit": "시간",
-                "factor": 0.025
-            }
-        }
+living_room = {
+    "air": {
+        "label": "에어컨",
+        "unit": "분",
+        "factor": 1.3 #kw 단위
     },
-
-    "trans": {
-        "label": "🚗 운송",
-        "emission_factor": 1,
-        "items": {
-            "car": {
-                "label": "자동차(휘발유)",
-                "unit": "L",
-                "factor": 19.731
-            },
-            "car_2": {
-                "label": "자동차(경유)",
-                "unit": "L",
-                "factor": 20.090
-            },
-            "plane": {
-                "label": "비행기",
-                "unit": "시간",
-                "factor": 900 * 285
-            }
-        }
+    "tv": {
+        "label": "TV",
+        "unit": "분",
+        "factor": 0.12
     },
-
-    "meal": {
-        "label": "🥗 채식",
-        "emission_factor": 1,
-        "items": {
-            "beef": {
-                "label": "소고기",
-                "unit": "g",
-                "factor": 0.03
-            },
-            "pork": {
-                "label": "돼지고기",
-                "unit": "g",
-                "factor": 0.01
-            },
-            "lamp": {
-                "label": "양",
-                "unit": "g",
-                "factor": 0.02
-            },
-            "chicken": {
-                "label": "닭",
-                "unit": "g",
-                "factor": 0.01
-            }
-        }
+    "light": {
+        "label": "조명",
+        "unit": "분",
+        "factor": 0.03
     },
-
-    "recycle": {
-        "label": "♻️ 재활용",
-        "emission_factor": 1,
-        "items": {
-            "can": {
-                "label": "캔",
-                "unit": "개",
-                "factor": 0.02
-            },
-            "plastic_bottle": {
-                "label": "플라스틱 병",
-                "unit": "병",
-                "factor": 0.02
-            }
-        }
+    "cleaner": {
+        "label": "청소기",
+        "unit": "분",
+        "factor": 0.6
+    },
+    "fan": {
+        "label": "선풍기",
+        "unit": "분",
+        "factor": 0.05
     }
 }
 
-selected_actions = {}
+kitchen = {
+    "cooker": {
+        "label": "전기밥솥",
+        "unit": "분",
+        "factor": 1
+    },
+    "refri": {
+        "label": "냉장고",
+        "unit": "분",
+        "factor": 0.1
+    },
+    "refri_k": {
+        "label": "김치냉장고",
+        "unit": "분",
+        "factor": 0.3
+    },
+    "microwave": {
+        "label": "전자레인지",
+        "unit": "분",
+        "factor": 0.8
+    },
+    "coffe_pot": {
+        "label": "커피포트",
+        "unit": "분",
+        "factor": 0.7
+    }
+}
 
-for category_key, category_info in actions.items():
+laundry_room = {
+    "washing_machine": {
+        "label": "세탁기",
+        "unit": "분",
+        "factor": 0.2
+    },
+    "iron": {
+        "label": "다리미",
+        "unit": "분",
+        "factor": 1.2
+    }
+}
 
-    if st.checkbox(category_info["label"]):
+recycle = { # 단위 = tCO2eq/t
+    "can": {
+        "label": "캔",
+        "unit": "개",
+        "factor": 9.6
+    },
+    "glass": {
+        "label": "유리병",
+        "unit": "병",
+        "factor": 1.2
+    },
+    "plastic": {
+        "label": "플라스틱",
+        "unit": "병",
+        "factor": 0.3
+    }
+}
 
-        for item_key, item_info in category_info["items"].items():
+meal = {
+    "beef": {
+        "label": "소고기",
+        "unit": "g",
+        "factor": 100
+    },
+    "pork": {
+        "label": "돼지고기",
+        "unit": "g",
+        "factor": 12
+    },
+    "chicken": {
+        "label": "닭고기",
+        "unit": "g",
+        "factor": 7
+    },
+    "seafood": {
+        "label": "해산물",
+        "unit": "g",
+        "factor": 5
+    },
+    "vegetarian_diet": {
+        "label": "채식",
+        "unit": "g",
+        "factor": 1
+    }
+}
 
-            if st.checkbox(item_info["label"]):
+rooms = {
+    "거실": living_room,
+    "주방": kitchen,
+    "다용도실": laundry_room,
+    "재활용": recycle,
+    "식사": meal
+}
 
-                value = st.number_input(
-                    f"{item_info['label']} ({item_info['unit']})",
-                    min_value=0.0,
-                    step=1.0,
-                    key=f"value_{category_key}_{item_key}"
-                )
+electric = {}
 
-                selected_actions[item_key] = {
-                    "value": value,
-                    "factor": item_info["factor"],
-                    "emission_factor": category_info["emission_factor"],
-                    "label": item_info["label"],
-                    "unit": item_info["unit"]
-                }
+electric.update(living_room)
+electric.update(kitchen)
+electric.update(laundry_room)
 
-if st.button("계산하기"):
+def create_inputs():
+
+    user_inputs = {}
+
+    for room_name, room in rooms.items():
+
+        st.header(room_name)
+
+        for key, info in room.items():
+
+            user_inputs[key] = st.number_input(
+                f'{info["label"]} ({info["unit"]})',
+                min_value=0,
+                key=key
+            )
+
+    return user_inputs
+
+def calculate_electric(user_inputs):
+
     total = 0
 
-    for action in selected_actions.values():
-        total += action["value"] * action["factor"] * action["emission_factor"]
+    for key, info in electric.items():
+        value = user_inputs[key]
+        factor = info["factor"]
 
-    st.success(f"총 탄소 절감량 : {total:.2f} kgCO₂")
+        total += value / 60 * factor * 0.4173 / 1000
+
+    return total
+
+def calculate_meal(user_inputs):
+
+    total = 0
+
+    return total
+
+def calculate_recycle(user_inputs):
+
+    total = 0
+
+    total += user_inputs["can"] * recycle["can"]["factor"] * 11 /1000000
+    total += user_inputs["glass"] * recycle["glass"]["factor"] * 400 / 1000000
+    total += user_inputs["plastic"] * recycle["plastic"]["factor"] * 15 / 1000000
+
+    return total
+
+def calculate_total(user_inputs):
+
+    electric = calculate_electric(user_inputs)
+    meal = calculate_meal(user_inputs)
+    recycle = calculate_recycle(user_inputs)
+
+    total = electric + meal - recycle
+
+    return total
+
+def show_result(total):
+
+    st.success(f"총 탄소배출량 : {total*1000:.2f} kgCO₂")
+
+user_inputs = create_inputs()
+
+if st.button("탄소배출량 계산"):
+
+    total = calculate_total(user_inputs)
+
+    show_result(total)
